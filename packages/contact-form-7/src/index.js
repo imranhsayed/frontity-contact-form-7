80c9e9cc4a9d69eaf22c511f946f0ebb64b98017
Imran Sayed <imranhsayed@gmail.com>
2019-09-29 23:09:16 +0530

    Add actions for contact form7

--- packages/contact-form-7/src/index.js
+++ packages/contact-form-7/src/index.js
@@ -1,4 +1,5 @@
 
+
 const cf7Inputs = {
 	name: "cf7Inputs",
 	test: node => node.component === "input" && /wpcf7-form-control/.test(node.props.className),
@@ -22,5 +23,27 @@ export default {
 		html2react: {
 			processors: [cf7Form, cf7Inputs]
 		}
+	},
+
+	actions: {
+		cf7: {
+			sendForm: ( { state } ) => async data => {
+				const res                          = await fetch( `https://orionhive.com/wp-json/contact-form-7/v1/contact-forms/${ data }/feedback` );
+				const body                         = await res.json();
+				// Populate state with the errors, or thank-you message...
+				state.cf7.forms[ data.id ].message = body.message;
+				if ( body.mail_sent ) {
+					state.cf7.forms[ data.id ].status  = "sent";
+					state.cf7.forms[ data.id ].message = body.message;
+				} else if ( body.validation_failed ) {
+					state.cf7.forms[ data.id ].status = "failed";
+					// Populate errors from the response so React components
+					// can see them and re-render appropriately...
+					state.cf7.forms[ data.id ].validationErrors = {
+						email: "The e-mail address entered is invalid."
+					};
+				}
+			}
+		}
 	}
-}
+};
