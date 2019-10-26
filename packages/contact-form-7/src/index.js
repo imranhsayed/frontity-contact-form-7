aa140973dab4748ae0defb581f285e477c352643
Imran Sayed <imranhsayed@gmail.com>
2019-10-26 17:57:03 +0530

    Add function doc comments

--- packages/contact-form-7/src/index.js
+++ packages/contact-form-7/src/index.js
@@ -1,11 +1,7 @@
 import { connect } from "frontity";
 import cf7Form from './processors/cf7Form';
 import cf7Inputs from "./processors/cf7Inputs";
-import cf7Spans from "./processors/cf7Spans";
 import cf7HiddenInputs from "./processors/cf7HiddenInputs";
-
-
-
 
 const MyForm = {
 
@@ -23,6 +19,12 @@ const MyForm = {
 	actions: {
 		cf7: {
 
+			/**
+			 * Initialize the form input object in the state.
+			 *
+			 * @param {Object} state State.
+			 * @return {Function}
+			 */
 			initForm: ( { state } ) => ( id ) => {
 
 				if ( ! state.cf7.forms[id] ) {
@@ -30,44 +32,76 @@ const MyForm = {
 				}
 			},
 
+			/**
+			 * Initialize the input values in the state.
+			 *
+			 * @param {Object} state State.
+			 * @return {Function}
+			 */
 			initInput: ( { state } ) => ( { id, inputName } ) => {
 					state.cf7.forms[id].inputVals = ( '' !== inputName ) ? { [inputName]: '' } : {};
 			},
 
+			/**
+			 * Handle on change event when user enters values in the form.
+			 *
+			 * Set the input value entered by the user in the state.
+			 *
+			 * @param {Object} state State.
+			 * @return {Function}
+			 */
 			changeInputValue: ( { state } ) => ( { id, inputName, value } ) => {
 				state.cf7.forms[id].inputVals[inputName] = value;
 			},
 
+			/**
+			 * Add hidden input values.
+			 *
+			 * @param {Object} state State.
+			 * @return {Function}
+			 */
 			addHiddenInputs: ( { state } ) => ( { id, inputName, value } ) => {
 				state.cf7.forms[id].inputVals[inputName] = value;
 			},
 
+			/**
+			 * Handle form submit.
+			 *
+			 * @param {Object} state State.
+			 * @return {Function}
+			 */
 			sendForm: ( { state } ) => async id => {
 
 				const myData = state.cf7.forms[id].inputVals;
+
+				// Create new form data to send the post request with form data.
 				let formData = new FormData();
 
 				Object.keys( myData  ).forEach( ( key ) => { 
 					formData.append( key, myData[ key ] ) ;
 				});
 
+				// CF7 REST API URL.
 				const url = `https://smitpatadiya.com/wp-json/contact-form-7/v1/contact-forms/${id}/feedback`;
 
+				// Post Request.
 				const res = await fetch( url, {
 					method: 'POST',
 					body: formData
 				} );
 				const body = await res.json();
-				console.warn( 'body', body );
 
 				/**
 				 * Populate state with the errors, or thank-you message...
 				 */
-
 				if ( 'mail_sent' === body.status ) {
 
 					state.cf7.forms[ id ].status  = "sent";
 					state.cf7.forms[ id ].message = body.message;
+
+					// Once the email is sent, clear the form fields.
+					console.warn( 'sdsd', state.cf7.forms[ id ].inputVals );
+					state.cf7.forms[ id ].inputVals = {};
 
 				} else if ( 'validation_failed' === body.status ) {
 
