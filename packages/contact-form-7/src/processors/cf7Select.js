f816e374138872bf112317bf106d6b6cd2691880
DarkChris86 <kormos.krisztian.sandor@gmail.com>
2020-04-02 16:00:14 +0200

    Update cf7Select.js

--- packages/contact-form-7/src/processors/cf7Select.js
+++ packages/contact-form-7/src/processors/cf7Select.js
@@ -4,7 +4,7 @@ const cf7Select = {
 
 	name: "cf7Select",
 	test: node => node.component === "select" && /wpcf7-form-control/.test( node.props.className ),
-	process: node => {
+	processor: node => {
 
 		const ariaInvalid     = ( 'undefined' === typeof ( node.props[ 'aria-invalid' ] ) ) ? '' : node.props[ 'aria-invalid' ];
 		const className       = ( 'undefined' === typeof ( node.props.className ) ) ? '' : node.props.className;
