9f06cd6cf270ad7a0e2c39558ae6c536b8f2d446
DarkChris86 <kormos.krisztian.sandor@gmail.com>
2020-04-04 11:09:47 +0200

    Update cf7Form.js

--- packages/contact-form-7/src/processors/cf7Form.js
+++ packages/contact-form-7/src/processors/cf7Form.js
@@ -2,11 +2,11 @@ import Form from "./../components/Form";
 
 const cf7Form = {
 	name: "cf7Form",
-	test: node =>
+	test: ({ node }) =>
 
 		node.component === "form" && node.props.className === "wpcf7-form",
 
-	process: node => {
+	processor: ({ node }) => {
 
 		node.props.id  = node.children[ 0 ].children[ 0 ].props.value;
 		node.component = Form;
